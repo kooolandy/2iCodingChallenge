192e7cb0f0730b6dd7a4e89e30aadf0d585ffea2
AndyK <AndyK@DESKTOP-E2F1AC9>
2023-11-08 17:16:09 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text.Json.Nodes;
+using System.Xml;
+
+Console.WriteLine("Enter X value :");
+var X = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Array :");
+var arr = Console.ReadLine();
+var split = arr.Split(',');
+
+List<int> ints = new List<int>();
+foreach (var a in split)
+{
+    var usable = int.Parse(a);
+    if (usable <= X)
+    {
+        Console.WriteLine($"Usable: {usable}");
+        ints.Add(usable);
+    }
+}
+
+var outVal = 0;
+var l = ints.Count();
+for (var i = 0; i < l; i++)
+{
+    for (var j = i +1; j < l; j++)
+    {
+        Console.WriteLine($"i: {i}, j: {j},  ");
+
+        if (X.Equals(ints[i] + ints[j]))
+        {
+            outVal++;
+        }
+    }    
+}
+
+Console.WriteLine($"Output: {outVal}");
+
---
+++ ReadMe.txt
@@ -0,0 +1,30 @@
+﻿First assumption array in order
+No limit to size of array sepecified but let's choose 6
+
+Examples
+ie { 1,2,3,4,5,6 } => 16
+
+First thing I noticed is that if the two largest dont add up to the X integer then the output is 0 pairs
+Also if the lowest number is greater than X there are 0 pairs
+
+X: 1
+Array: [3, 4, 5, 6]
+Output: 0 
+
+X: 15
+Array: [0, 15, 32, 2000, 15000]
+Output: 1
+
+X: 42
+Array: [1, 1, 10, 32, 41]
+Output: 2
+
+From looking at the data any numbers larger than X can be ignored (assuming no negative whole numbers)
+
+Was going to create test data in json and then realised I am on a time limit ;-)
+
+1. First step was to parse input and validate usable values in array
+2. Then to loop thru 
+
+Running my code I would question the result from X = 42
+	1+41 = 42, 1+ 41 = 42, and 10+32=42 => 3 
